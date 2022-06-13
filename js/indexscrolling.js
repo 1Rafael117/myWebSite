400e46a7d880a71ac43c534001cc149076458d38
1Rafael117 <rafael_perez_in@comunidad.unam.mx>
2022-06-13 01:48:41 -0500

    start-scrolling

---
+++ js/indexscrolling.js
@@ -0,0 +1,12 @@
+window.sr  = ScrollReveal();
+
+sr.reveal('.level', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+});
+sr.reveal('.divcent', {
+    duration: 1000,
+    origin: 'left',
+    distance: '-100px'
+});
